22ab7aaedc386cb3b3cb8435b4671aa41cbc5668
Matthew Greenspan <matthewgreenspan06@gmail.com>
2026-07-29 23:26:33 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        minPrice = prices[0]
+
+        for price in prices:
+            minPrice = min(price, minPrice)
+            maxProfit = max(maxProfit, price - minPrice)
+        return maxProfit
+
+
